3b3c5d1e62121c34ab3dfd1c45e54a182cd09d71
Melvin Batista <mbatista@infrasystpr.com>
2022-10-16 12:45:58 -0400

    emitting custom events

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="backdrop">
+  <div class="backdrop" @click="closeModal">
     <div class="modal" :class="{ sale: theme === 'sale' }">
       <h1>{{ header }}</h1>
       <p>{{ text }}</p>
@@ -10,6 +10,11 @@
 <script>
 export default {
   props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
 };
 </script>
 
@@ -38,10 +43,10 @@ export default {
 }
 
 .modal.sale {
-    background: crimson;
-    color: white;
+  background: crimson;
+  color: white;
 }
 .modal.sale h1 {
-    color: white;
+  color: white;
 }
 </style>

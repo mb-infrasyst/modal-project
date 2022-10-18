c03677317800a9e2d2630b67890350ffce841f0e
Melvin Batista <mbatista@infrasystpr.com>
2022-10-17 20:12:46 -0400

    slots

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="backdrop" @click.self="closeModal">
     <div class="modal" :class="{ sale: theme === 'sale' }">
-      <h1>{{ header }}</h1>
-      <p>{{ text }}</p>
+      <slot>default content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["header", "text", "theme"],
+  props: ["theme"],
   methods: {
     closeModal() {
       this.$emit("close");
@@ -42,11 +44,33 @@ export default {
   font-style: normal;
 }
 
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10pm 0;
+}
+
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
 .modal.sale {
   background: crimson;
   color: white;
 }
 .modal.sale h1 {
+  color: white;
+}
+
+.model.sale .actions {
+  color: white;
+}
+
+.modal.sale .actions a {
   color: white;
 }
 </style>
